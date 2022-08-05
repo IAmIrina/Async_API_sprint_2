d0ae332fffbbcc80b6109d4a1f3faaf36b244551
IAmIrina <irina.vl.kiseleva@gmail.com>
2022-08-05 12:02:27 +0000

    Add PaginationParams model

--- fastapi_movies/src/api/v1/films.py
+++ fastapi_movies/src/api/v1/films.py
@@ -6,7 +6,8 @@ from enum import Enum
 from services.film import FilmService, get_film_service
 from models.film import Films, Film
 from core.messages import FILM_NOT_FOUND
-from models.custom_models import SortModel
+from models.custom_models import SortModel, PaginateParams
+
 
 router = APIRouter()
 
@@ -24,8 +25,7 @@ async def films(
     film_service: FilmService = Depends(get_film_service),
     sort: SortingImdbRating = Query(default=SortingImdbRating.desc,
                                     description='IMDB rating sort.'),
-    page_num: int = Query(default=1, ge=1, alias='page[number]', description='Page number.'),
-    page_size: int = Query(default=50, ge=1, le=100, alias='page[size]', description='Page size.'),
+    paginated_params: PaginateParams = Depends(),
     filter: str = Query(
         default=None,
         alias='filter[genre]',
@@ -36,7 +36,7 @@ async def films(
         sort = SortModel(field='imdb_rating', order=sort.__dict__['_name_'])
     else:
         sort = None
-    films = await film_service.get_page(page_num, page_size, sort, filter)
+    films = await film_service.get_page(paginated_params.page_num, paginated_params.page_size, sort, filter)
 
     if not films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=FILM_NOT_FOUND)
@@ -46,11 +46,10 @@ async def films(
 @router.get('/search', response_model=Films, summary='Full text search.', description='Use query for full text search.')
 async def films_search(
     query: str,
-    page_num: int = Query(default=1, ge=1, alias='page[number]', description='Page number.'),
-    page_size: int = Query(default=50, ge=1, le=100, alias='page[size]', description='Page size.'),
+    paginated_params: PaginateParams = Depends(),
     film_service: FilmService = Depends(get_film_service),
 ) -> Films:
-    films = await film_service.full_text_search(page_num, page_size, query)
+    films = await film_service.full_text_search(paginated_params.page_num, paginated_params.page_size, query)
     if not films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=FILM_NOT_FOUND)
 
--- fastapi_movies/src/models/custom_models.py
+++ fastapi_movies/src/models/custom_models.py
@@ -1,9 +1,10 @@
 """Mixins pydantic models."""
 
 import orjson
-from typing import Generic, List, TypeVar, Literal
+from typing import Generic, List, TypeVar, Literal, Optional
 from pydantic import BaseModel
 from pydantic.generics import GenericModel
+from fastapi import Query
 
 Model = TypeVar("Model", bound=BaseModel)
 
@@ -26,6 +27,20 @@ class PaginationModel(GenericModel, Generic[Model]):
     page_num: int
     page_size: int
     data: List[Model]
+
+
+class PaginateParams:
+    def __init__(
+        self,
+        page_size: Optional[int] = Query(
+            50, alias='page[size]', description='Items amount on page', ge=1
+        ),
+        page_number: Optional[int] = Query(
+            1, alias='page[number]', description='Page number for pagination', ge=1
+        ),
+    ):
+        self.page_num = page_number
+        self.page_size = page_size
 
 
 class SortModel(BaseModel):
